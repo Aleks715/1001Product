f21d7c141a5a6774364b3847b7b3c0fe7d7b08d2
Aleks <aleksiguliashvili77@gmail.com>
2026-06-24 13:45:56 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const select = document.getElementById('product-select');
+    const priceDisplay = document.getElementById('price-display');
+    
+    if (select) {
+        select.addEventListener('change', function() {
+            priceDisplay.innerText = "ფასი: " + this.value + " ₾";
+        });
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const select = document.getElementById('product-select');
+    const priceDisplay = document.getElementById('price-display');
+    const form = document.querySelector("form");
+
+    if (select) {
+        select.addEventListener('change', function() {
+            priceDisplay.innerText = "ფასი: " + this.value.split("—")[1];
+        });
+    }
+
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault(); // აღარ გადაგიყვანს სხვა გვერდზე
+
+        const phone = document.querySelector('input[name="phone"]').value;
+
+        fetch(form.action, {
+            method: "POST",
+            body: new FormData(form),
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(() => {
+
+            form.innerHTML = `
+                <div class="success-message">
+                    <h2>მადლობა შეკვეთისთვის ❤️</h2>
+                    <p>ჩვენ მალე დაგიკავშირდებით.</p>
+                </div>
+            `;
+
+        })
+        .catch(() => {
+            alert("დაფიქსირდა შეცდომა, სცადეთ თავიდან");
+        });
+
+    });
+
+});
+
+
+const images = document.querySelectorAll(".gallery img");
+const viewer = document.getElementById("image-viewer");
+const bigImage = document.getElementById("big-image");
+const close = document.getElementById("close");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        viewer.style.display = "flex";
+        bigImage.src = img.src;
+    });
+});
+
+close.onclick = () => {
+    viewer.style.display = "none";
+};
+
+viewer.onclick = (e) => {
+    if(e.target === viewer){
+        viewer.style.display = "none";
+    }
+};
